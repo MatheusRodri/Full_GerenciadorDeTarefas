7dfe2d471a72a737d15d3919d4bc160842b5e055
Matheus Rodrigues De Jesus <matheus.rj25@hotmail.com>
2024-11-17 11:40:56 -0300

    feat: add route to create a new task

--- Backend/index.js
+++ Backend/index.js
@@ -8,12 +8,30 @@ const TaskModel = require('./src/models/task.model');
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 
 connectToDatabase();
 
 app.get('/tasks', async (req, res) => {
-    const tasks = await TaskModel.find({})
-    res.status(200).send(tasks);
+    try {
+        const tasks = await TaskModel.find({})
+        res.status(200).send(tasks);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+})
+
+
+app.post('/tasks', async (req, res) => {
+    try {
+        const newTask = new TaskModel(req.body);
+
+        await newTask.save()
+
+        res.status(201).send(newTask);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
 })
 
 app.listen(8000, () => { console.log('Server is running on port 8000') })
