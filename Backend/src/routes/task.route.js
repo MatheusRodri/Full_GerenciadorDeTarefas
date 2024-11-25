1b4da7483006dc2ed142e7065c52095d1ec1aeec
Matheus Rodrigues De Jesus <matheus.rj25@hotmail.com>
2024-11-25 20:32:21 -0300

    feat: move get tasks to a controller

--- Backend/src/routes/task.route.js
+++ Backend/src/routes/task.route.js
@@ -1,15 +1,12 @@
 const express = require('express');
+
 const TaskModel = require('../models/task.model');
+const TaskController = require('../controllers/task.controller');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    try {
-        const tasks = await TaskModel.find({})
-        res.status(200).send(tasks);
-    } catch (e) {
-        res.status(500).send(e.message);
-    }
+    return new TaskController(req, res).getTasks();
 })
 
 router.get('/:id', async (req, res) => {
